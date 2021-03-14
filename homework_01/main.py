aa0c5ef3c6006f3e4f103c7cd8c66d25401dfc00
vakhrushevkirill <vakhrushev.kirill93@yandex.ru>
2021-03-14 08:51:34 +0300

    Перовое ДЗ

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,12 +4,12 @@
 """
 
 
-def power_numbers():
+def power_numbers(*args):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     """
-
+    return list(map(lambda x: x ** 2, args))
 
 # filter types
 ODD = "odd"
@@ -17,9 +17,32 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def filter_numbers(list_numbers, filter_type=PRIME):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
     (выбор производится передачей дополнительного аргумента)
     """
+    if filter_type == ODD:
+        return list(filter(lambda x: x % 2 == 1, list_numbers))
+    elif filter_type == EVEN:
+        return list(filter(lambda x: x % 2 == 0, list_numbers))
+    elif filter_type == PRIME:
+        temp_list_number = list()
+        for number in list_numbers:
+            if number == 1:
+                continue
+            elif number > 1:
+                it_prime = False
+                for denominator in range(1,number):
+                    if number % denominator == 0 and denominator != 1 and denominator != number:
+                        #list_numbers.remove(number) - Так было бы лучше, но валится тест из за того, что список меняется на лету.
+                        it_prime = False
+                        break
+                    else:
+                        it_prime = True
+                if it_prime == True:
+                    temp_list_number.append(number)
+        return temp_list_number
+
+print(filter_numbers([3, 5, 7, 9, 11], PRIME))
