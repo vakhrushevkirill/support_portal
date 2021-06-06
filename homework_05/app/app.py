6424baba421331aac7988684b71d22bac0e51ef3
vakhrushevkirill <vakhrushev.kirill93@yandex.ru>
2021-06-06 13:59:55 +0300

    Homework_05

---
+++ homework_05/app/app.py
@@ -0,0 +1,26 @@
+"""
+Домашнее задание №4
+Первое веб-приложение
+
+создайте базовое приложение на Flask
+создайте index view /
+добавьте страницу /about/, добавьте туда текст
+создайте базовый шаблон (используйте https://getbootstrap.com/docs/5.0/getting-started/introduction/#starter-template)
+в базовый шаблон подключите статику Bootstrap 5 и добавьте стили, примените их
+в базовый шаблон добавьте навигационную панель nav (https://getbootstrap.com/docs/5.0/components/navbar/)
+в навигационную панель добавьте ссылки на главную страницу / и на страницу /about/ при помощи url_for
+"""
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about/')
+def about():
+    hello = "Осваиваем новые горизонты"
+    return render_template('about.html', hello=hello)
